79c261bb37a365d783da3d91a3d28f2c7d7a3247
wuyaheng <wuyaheng2016@gmail.com>
2020-08-09 09:39:43 -0400

    set up react and fetch data

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# progressive-web-application
+practice pwa
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: '',
+        }
+    });
+
+    return data;
+}
